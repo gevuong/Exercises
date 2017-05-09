8721f296046d79e74904cf9ab5c524dae09ebca3
George V <georgevuong18@gmail.com>
2017-05-09 15:58:11 -0700

    completed times and divide in rpn calc

--- 341a4778-georgevuong18-rpn-calculator/lib/00_rpn_calculator.rb
+++ 341a4778-georgevuong18-rpn-calculator/lib/00_rpn_calculator.rb
@@ -22,7 +22,18 @@ class RPNCalculator
     first_num = @stack.pop
     res_minus = first_num - second_num
     @stack << res_minus
-
-  
   end
+
+  def divide
+    divisor = @stack.pop.to_f
+    dividend = @stack.pop
+    res_divide = dividend / divisor
+    @stack << res_divide
+  end
+
+  def times
+    res_times = @stack.pop * @stack.pop
+    @stack << res_times
+  end
+
 end
