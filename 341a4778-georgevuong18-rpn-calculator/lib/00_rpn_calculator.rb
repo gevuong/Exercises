e0bcbb3ce7fb77b0816242f2f4da11ea4c3a4781
George V <georgevuong18@gmail.com>
2017-05-09 15:43:32 -0700

    completed plus and minus in rpn calc

--- 341a4778-georgevuong18-rpn-calculator/lib/00_rpn_calculator.rb
+++ 341a4778-georgevuong18-rpn-calculator/lib/00_rpn_calculator.rb
@@ -1,3 +1,28 @@
 class RPNCalculator
-  # TODO: your code goes here!
+
+  def initialize
+    @stack = []
+  end
+
+  def push(int)
+    @stack << int
+  end
+
+  def plus
+    res_sum = @stack.pop + @stack.pop
+    @stack << res_sum
+  end
+
+  def value
+    @stack.last
+  end
+
+  def minus
+    second_num = @stack.pop
+    first_num = @stack.pop
+    res_minus = first_num - second_num
+    @stack << res_minus
+
+  
+  end
 end
