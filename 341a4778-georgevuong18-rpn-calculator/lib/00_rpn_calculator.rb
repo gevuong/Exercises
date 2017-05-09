a937e69fa1ad408067c3e5bdaf0455ac52f2f98f
George V <georgevuong18@gmail.com>
2017-05-09 16:16:28 -0700

    raise error when stack is less than 2

--- 341a4778-georgevuong18-rpn-calculator/lib/00_rpn_calculator.rb
+++ 341a4778-georgevuong18-rpn-calculator/lib/00_rpn_calculator.rb
@@ -9,6 +9,7 @@ class RPNCalculator
   end
 
   def plus
+    raise "calculator is empty" if @stack.count <= 1
     res_sum = @stack.pop + @stack.pop
     @stack << res_sum
   end
@@ -18,6 +19,7 @@ class RPNCalculator
   end
 
   def minus
+    raise "calculator is empty" if @stack.count <= 1
     second_num = @stack.pop
     first_num = @stack.pop
     res_minus = first_num - second_num
@@ -25,6 +27,7 @@ class RPNCalculator
   end
 
   def divide
+    raise "calculator is empty" if @stack.count <= 1
     divisor = @stack.pop.to_f
     dividend = @stack.pop
     res_divide = dividend / divisor
@@ -32,6 +35,7 @@ class RPNCalculator
   end
 
   def times
+    raise "calculator is empty" if @stack.count <= 1
     res_times = @stack.pop * @stack.pop
     @stack << res_times
   end
