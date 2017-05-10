c312690cd3373687ad69407e1f7d2cc0e735d386
George V <georgevuong18@gmail.com>
2017-05-09 17:56:14 -0700

    completed tokenize string

--- 341a4778-georgevuong18-rpn-calculator/lib/00_rpn_calculator.rb
+++ 341a4778-georgevuong18-rpn-calculator/lib/00_rpn_calculator.rb
@@ -1,7 +1,28 @@
 class RPNCalculator
+  def tokens(str)
+
+    str.split.each do |ch|
+      if "0123456789".include?(ch)
+        @tokens << ch.to_i
+      elsif "-+*/".include?(ch)
+        @tokens << ch.to_sym
+      end
+    end
+
+    @tokens
+  end
+
+  def evaluate(rpn)
+    rpn.split.select { |ch| @stack << ch.to_i if "0123456789".include?(ch) }
+    rpn.split.reject { |ch| @operator << ch if "0123456789".include?(ch) }
+
+    plus if rpn == "+"
+  end
 
   def initialize
     @stack = []
+    @operator = []
+    @tokens = []
   end
 
   def push(int)
