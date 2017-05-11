7755d3f27cccdc0f67c180bf3269b3b533e25826
George V <georgevuong18@gmail.com>
2017-05-11 16:13:47 -0700

    completed 5 of 7 specs in student.rb

--- 341a4778-georgevuong18-students-courses/lib/student.rb
+++ 341a4778-georgevuong18-students-courses/lib/student.rb
@@ -1,3 +1,30 @@
 class Student
+  #attr_reader :courses, :first_name, :last_name
 
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+    @courses = []
+  end
+
+  def enroll(new_course)
+    @courses << new_course unless @courses.include?(new_course)
+    #course.student << self
+  end
+
+  def courses
+    @courses
+  end
+
+  def first_name
+    @first_name.to_s
+  end
+
+  def last_name
+    @last_name.to_s
+  end
+
+  def name
+    @first_name.to_s + " " + @last_name.to_s
+  end
 end
