e38606d12fd2b4af5da20057ced042a2fec3fa36
George V <georgevuong18@gmail.com>
2017-05-15 10:32:42 -0700

    completed rspec1 and pigs_latin.rb

--- 341a4778-georgevuong18-rspec-1/lib/04_pig_latin.rb
+++ 341a4778-georgevuong18-rspec-1/lib/04_pig_latin.rb
@@ -1,19 +1,22 @@
 def translate(word)
+  arr = []
   vowels = "aeiou"
   consonants = "bcdfghjklmnpqrstvwxyz"
-  first_letter = word.chars[0]
-  second_letter = word.chars[1]
-  third_letter = word.chars[2]
 
-  if vowels.include?(first_letter)
-    word + "ay"
-  elsif consonants.include?(first_letter) && consonants.include?(second_letter) && consonants.include?(third_letter)
-
-    word.chars[3..-1].join + first_letter + second_letter + third_letter + "ay"
-  elsif consonants.include?(first_letter) && consonants.include?(second_letter)
-    word.chars[2..-1].join + first_letter + second_letter + "ay"
-  elsif consonants.include?(first_letter)
-    word.chars[1..-1].join + first_letter + "ay"
-
+  word.split.each do |words|
+    first_letter = words.chars[0]
+    second_letter = words.chars[1]
+    third_letter = words.chars[2]
+    if vowels.include?(first_letter)
+      arr << words + "ay"
+    elsif consonants.include?(first_letter) && consonants.include?(second_letter) && consonants.include?(third_letter) || (consonants.include?(first_letter) && second_letter == "q" && third_letter == "u")
+      arr << words.chars[3..-1].join + first_letter + second_letter + third_letter + "ay"
+    elsif (first_letter == "q" && second_letter == "u") || consonants.include?(first_letter) && consonants.include?(second_letter)
+      arr << words.chars[2..-1].join + first_letter + second_letter + "ay"
+    elsif consonants.include?(first_letter)
+      arr << words.chars[1..-1].join + first_letter + "ay"
+    end
   end
+
+  arr.join(" ")
 end
