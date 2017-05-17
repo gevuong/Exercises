be8a2b5089fcf330ac4153c9d685ff06f04c2dd5
George V <georgevuong18@gmail.com>
2017-05-16 22:53:52 -0700

    completed billions in number_in_words.rb

--- 341a4778-georgevuong18-numbers-in-words/lib/01_in_words.rb
+++ 341a4778-georgevuong18-numbers-in-words/lib/01_in_words.rb
@@ -3,15 +3,73 @@ class Integer
   def in_words
     #@numbers_in_words = numbers_in_words
     return "zero" if self == 0
-    if self >= 1000 && self <= 999999
+    if self >= 10**12 && self < 10**13
+      trillions_in_words
+    elsif self >= 10**9 && self < 10**12
+      billions_in_words
+    elsif self >= 10**6 && self < 10**9
+      millions_in_words
+    elsif self >= 1000 && self < 10**6
       thousands_in_words
     elsif self >= 100 && self <= 999
-      hundreds_in_words
+      hundreds_in_words(self)
     elsif self >= 20 && self < 100
       tens_in_words(self)
     else
       less_than_20_in_words
     end
+  end
+
+  def trillions_in_words
+    remainder = self % 10**12
+    if remainder == 0
+      word = numbers_in_words(self / 10**12) + " " + "trillion"
+    elsif
+      trillions = self / 10**12
+      billions = self % 10**12 / 10**9
+      millions = self % 10**9 / 10**6
+      thousands = self % 10**6 / 1000
+      hundreds = self % 10**3 / 100
+      tens = self % 100 / 10
+      ones = self % 10
+      word = numbers_in_words(trillions) + " " + "trillion" + " " + hundreds_in_words(billions) + " " + "billion" + " " + hundreds_in_words(millions) + " " + "million" + hundreds_in_words(thousands) + " " + "thousand" + " " + numbers_in_words(hundreds) + " "
+      + numbers_in_words(tens) + " " + numbers_in_words(ones)
+    end
+
+    word
+  end
+
+  def billions_in_words
+    remainder = self % 10**9
+    if remainder == 0
+      word = number_in_words(self / 10**9) + " " + "billion"
+    elsif
+      billions = self / 10**9
+      millions = self % 10**9 / 10**6
+      thousands = self % 10**6 / 1000
+      hundreds = self % 10**3 / 100
+      tens = self % 100 / 10 * 10
+      ones = self % 10
+      word = numbers_in_words(billions) + " " + "billion" + " " + hundreds_in_words(millions) + " " + "million" + " " +  hundreds_in_words(thousands) + " " + "thousand" + " " + numbers_in_words(hundreds) + " " + "hundred"  + " " + numbers_in_words(tens)
+    end
+
+    word
+  end
+
+  def millions_in_words
+    remainder = self % 10**6
+    if remainder == 0
+      word = number_in_words(self / 10**6) + " " + "million"
+    elsif
+      millions = self / 10**6
+      thousands = self % 10**6 / 1000
+      hundreds = self % 1000 / 100
+      tens = self % 100 / 10
+      ones = self % 10
+      word = numbers_in_words(millions) + " " + "million" + " " + numbers_in_words(ones)
+    end
+
+    word
   end
 
   def thousands_in_words
@@ -29,17 +87,17 @@ class Integer
     word
   end
 
-  def hundreds_in_words
-    remainder = self % 100
+  def hundreds_in_words(int)
+    remainder = int % 100
     if remainder == 0
-      word = numbers_in_words(self / 100) + " " + "hundred"
+      word = numbers_in_words(int / 100) + " " + "hundred"
     elsif remainder < 20 && remainder > 0
-      hundreds = self / 100
+      hundreds = int / 100
       word = numbers_in_words(hundreds) + " " + "hundred" + " " + numbers_in_words(remainder)
     else
-      ones = self % 10
-      tens = self % 100 / 10 * 10
-      hundreds = self / 100
+      ones = int % 10
+      tens = int % 100 / 10 * 10
+      hundreds = int / 100
       word = numbers_in_words(hundreds) + " " + "hundred" + " " + numbers_in_words(tens) + " " + numbers_in_words(ones)
     end
 
@@ -64,7 +122,7 @@ class Integer
   end
 
   def numbers_in_words(int)
-    number_in_word = { 0 => "zero", 1 => "one", 2 => "two", 3 =>     "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven",
+    number_in_word = { 0 => "zero", 1 => "one", 2 => "two", 3 => "three",  4 => "four", 5 => "five", 6 => "six", 7 => "seven",
               8 => "eight", 9 => "nine", 10 => "ten", 11 => "eleven",
              12 => "twelve", 13 => "thirteen", 14 => "fourteen",
              15 => "fifteen", 16 => 'sixteen', 17 => 'seventeen',
