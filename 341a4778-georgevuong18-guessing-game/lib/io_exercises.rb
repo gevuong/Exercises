474235a5b3b2247bd27efbf62c136d1c165c2cd2
George V <georgevuong18@gmail.com>
2017-05-18 17:55:32 -0700

    completed 8 of 10 specs in guessing_game

--- 341a4778-georgevuong18-guessing-game/lib/io_exercises.rb
+++ 341a4778-georgevuong18-guessing-game/lib/io_exercises.rb
@@ -1,5 +1,4 @@
 # I/O Exercises
-#
 # * Write a `guessing_game` method. The computer should choose a number between
 #   1 and 100. Prompt the user to `guess a number`. Each time through a play loop,
 #   get a guess from the user. Print the number guessed and whether it was `too
@@ -10,3 +9,27 @@
 #   shuffles the lines, and saves it to the file "{input_name}-shuffled.txt". You
 #   could create a random number using the Random class, or you could use the
 #   `shuffle` method in array.
+
+  def guessing_game
+    answer = rand(100)
+    counter = 0
+    # prompt user to guess a number
+    puts "Guess a number between 1 and 100"
+    user_input = gets.chomp
+    puts user_input
+     raise NoMoreInput if user_input == 0
+    # loop do
+    #   user_input = gets.chomp
+    #   puts user_input
+    #   if user_input > answer
+    #     counter += 1
+    #     puts "#{user_input} is too high, guess again"
+    #   elsif user_input < answer
+    #     counter += 1
+    #     puts "#{user_input} is too low, guess again"
+    #   else
+    #     puts " #{user_input} is right! And it took #{counter} guesses"
+    #     break
+    #   end
+    # end
+  end
